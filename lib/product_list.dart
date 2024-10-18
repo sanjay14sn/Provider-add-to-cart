4f68ab920a4045bf7938798ef6b5b6175fe7e528
sanjay14sn <snsanjay2002@gmail.com>
2024-10-18 13:54:33 +0530

    new commit

---
+++ lib/product_list.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:untitled8/cart_model.dart';
+import 'package:untitled8/cart_provider.dart';
+import 'package:untitled8/db_helper.dart';
+import 'package:badges/badges.dart' as badges ;
+
+
+class ProductListScreen extends StatefulWidget {
+  const ProductListScreen({super.key});
+
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+
+  DBHelper? dbHelper = DBHelper();
+
+
+  List<String> productName = ['Donuts', 'Pizza', 'Cake', 'Grapes', 'Apple', 'Orange'];
+  List<String> productUnit = ['Piece', 'Piece', 'Piece', 'KG', 'KG', 'KG'];
+  List<int> productPrice = [50, 60, 70, 80, 90, 100];
+  List<String> productImage = [
+    'assetss/donuts.jpg.jpg',
+    'assetss/pizza.jpg.jpg',
+    'assetss/cake.jpg.jpg',
+    'assetss/Grapes.jpg.jpg',
+    'assetss/Apple.jpg.jpg',
+    'assetss/Orange.jpg.jpg'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<CartProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product List'),
+        centerTitle: true,
+        actions: [
+          Center(
+            child: badges.Badge(
+              badgeContent: Consumer<CartProvider>(builder:(context,value,child){
+                return Text(value.getCounter().toString(),style:TextStyle(color:Colors.white),);
+              },
+                  ),
+              badgeAnimation:badges.BadgeAnimation.slide(
+              animationDuration:Duration(milliseconds:200)),
+              child: IconButton(
+                icon: const Icon(Icons.shopping_bag),
+                onPressed: () {
+                  // Action for the shopping cart button
+                },
+              ),
+            ),
+          ),
+          const SizedBox(width: 20.0),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: productName.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image(
+                          height: 100,
+                          width: 100,
+                          image: AssetImage(productImage[index]),
+                        ),
+                        const SizedBox(width: 10), // Add space between image and text
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                productName[index],
+                                style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                '${productUnit[index]}  \$${productPrice[index]}',
+                                style: const TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.w500, color: Colors.green),
+                              ),
+                            ],
+                          ),
+                        ),
+                        //Spacer(), // This will push the button to the right
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: InkWell(
+                            onTap: () {
+                              dbHelper!
+                                  .insert(
+                                Cart(
+                                    id: index,
+                                    productId: index.toString(),
+                                    productName: productName[index],
+                                    initialPrice: productPrice[index],
+                                    productPrice: productPrice[index],
+                                    quantity: 1,
+                                    unitTag: productUnit[index],
+                                    image: productImage[index]
+                                ),
+                              )
+                                  .then((value) {
+                                    print("Product Added to Cart");
+                                    cart.addTotalPrice(double.parse(productPrice[index].toString()));
+                                    cart.addCounter();
+
+                                
+                                
+                              })
+                                  .catchError((error) {
+                                print(error.toString());
+                              });
+                              },
+                          child: Container(
+                            height: 35,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Add To Cart',
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
